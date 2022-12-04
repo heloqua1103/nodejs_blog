014751369aa10ad121bdaebd5ff97d96e828b663
Hieu <nguyenhieu11032002@gmail.com>
2022-12-04 16:48:35 +0700

    04/12

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 const path = require('path');
 const express = require('express');
+const methodOverride = require('method-override')
 var morgan = require('morgan');
 const handlebars = require('express-handlebars');
+
+const route = require('./routes');
+const db = require('./config/db')
+
+// Connect to DB
+db.connect();
+
 const app = express();
 const port = 3002;
 
-const route = require('./routes');
-
+// Use static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(
@@ -16,6 +23,8 @@ app.use(
 );
 app.use(express.json());
 
+app.use(methodOverride('_method'))
+
 // HTTP logger
 // app.use(morgan('combined'))
 
@@ -24,14 +33,17 @@ app.engine(
     'hbs',
     handlebars.engine({
         extname: '.hbs',
+        helpers: {
+            sum: (a, b) => a + b
+        }
     }),
 );
 app.set('view engine', 'hbs');
-            app.set('views', path.join(__dirname, 'resources\\views'));
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 // Routes init
-            route(app);
+route(app);
 
-        app.listen(port, () => {
+app.listen(port, () => {
     console.log(`Example app listening on port http://localhost:${port}`);
 });
